f96cab0f20fe1dea3124ca137e3abeca59acbf37
Gonzo22003 <peter.gonzalez@stratus-tis.com>
2022-07-03 13:37:33 -0400

    update

--- js/index.js
+++ js/index.js
@@ -1,22 +1,23 @@
 $(function () {
     $('#example tfoot th').each(function () {
         var title = $(this).text();
-        $(this).html('<input type="text" placeholder="Search ' + title + '" />');})
+        $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+    })
  
-        var table = $('#example').DataTable({
-            initComplete: function () {
-                // Apply the search
-                this.api()
-                    .columns()
-                    .every(function () {
-                        var that = this;
+    var table = $('#example').DataTable({
+        initComplete: function () {
+            // Apply the search
+            this.api()
+            .columns()
+            .every(function () {
+                var that = this;
      
-                        $('input', this.footer()).on('keyup change clear', function () {
-                            if (that.search() !== this.value) {
-                                that.search($(this).val(),$(this)).draw();
-                            }
-                        });
-                    });
-            },
-        });
+                $('input', this.footer()).on('keyup change clear', function () {
+                    if (that.search() !== this.value) {
+                        that.search($(this).val(),$(this)).draw();
+                    }
+                });
+            });
+        },
+    });
 });
